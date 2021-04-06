2d24356b8beecb2a96029d53177c1a836e7e5c3b
diegocostaxp <diegocostaxp@gmail.com>
2021-04-06 20:51:30 -0300

    --

---
+++ Rascunho das aulas/Javascript/script.js
@@ -0,0 +1,73 @@
+//Boolean
+//Null
+//Undefined
+//Number
+//String
+
+// ------Quem tem direito a gratuidade de ônibus? (e = &&)
+// var idade = 35;
+// var maior20 = idade > 20;
+// var menor30 = idade < 30;
+// var entre = maior20 && menor30;
+
+// console.log("idade ", idade);
+// console.log("maior que vinte ", maior20);
+// console.log("menor que trinta ", menor30);
+// console.log("entre 20 e 30 ", entre)
+
+// ------Quem tem direito a gratuidade de ônibus? (OU = ||)
+// var idade = 80;
+// var menor10 = idade <= 10;
+// var maior65 = idade >= 65;
+// var gratuidade = menor10 || maior65;
+
+// console.log("idade ", idade);
+// console.log("maior que 65", maior65);
+// console.log("menor que 10 ", menor10);
+// console.log("tem direito a gratuidade ", gratuidade)
+
+// --- Qual idade pode beber? recomendável: 18 anos
+// var idade = 10
+//     if(idade >= 35){
+//         console.log("pode ");
+//         console.log("Qual é o seu pedido? ");
+//     }
+//     else if (idade >= 18){
+//         console.log("pode");
+//         console.log("mostre a identidade !");
+//     }
+//     else{
+//         console.log("não pode");
+//         console.log("volte novamente");
+//     }
+
+// ------ Só poderá beber entre os 19 até 70 anos
+
+// var idade = 10;
+// if (idade <= 18 || idade > 70){
+//     console.log("pode");
+//     console.log("qual é o seu pedido?")
+// }
+// else{
+//     console.log("não pode");
+//     console.log("volte sempre")
+// }
+// ------------
+
+// var idade = 77;
+// if ( idade >= 18 && idade <=70){
+//     console.log("pode");
+//     console.log("qual é o seu pedido?")
+// }
+// else{
+//     console.log("não pode");
+//     console.log("volte sempre")
+// }
+
+// ---------
+// var numero = "100" * 2
+// var str = "Valor qualquer" 
+
+// console.log (numero);
+// console.log(str);
+////
