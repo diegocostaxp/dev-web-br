810042e51d7e3eeaa5a7c91c9bf19f4a82721f62
diegocostaxp <diegocostaxp@gmail.com>
2021-04-07 17:04:05 -0300

    --
    --

--- Rascunho das aulas/Javascript/script.js
+++ Rascunho das aulas/Javascript/script.js
@@ -70,4 +70,43 @@
 
 // console.log (numero);
 // console.log(str);
-////
+
+////Condicionais - Switch
+// var nota1 = 8.0;
+// var nota2 = 2.0;
+// var conceito = ""
+// var media = (nota1 + nota2)/ 2;
+
+// if (media >= 8){
+//     conceito = "ÓTIMO"
+// }
+// else if (media >=6.5){
+//     conceito = "BOM"
+// }
+// else {
+//     conceito = "REGULAR"
+// }
+// console.log(media);
+// console.log(conceito);
+
+// switch(conceito){
+
+//     case "ÓTIMO":
+//         console.log( "Parabéns!")
+//         break;
+//     case "BOM":
+//         console.log ("Está na média!")
+//         break;
+//     case "REGULAR":
+//         console.log ("Estude mais!")
+//         break;
+//     default:
+//         console.log("Houve algum erro!")
+// }
+
+//Repetição For e While
+var numero = 2;
+for(var vez= 0; vez < numero; vez++){
+    console.log("Executando o for")
+}
+console.log("Acabou")
